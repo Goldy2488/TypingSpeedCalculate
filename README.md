bdfc3b43dc219b09d27c636848d87b1f1cac6c4b
Goldy2488 <goldy22@navgurukul.org>
2023-09-16 20:27:11 +0530

    change commit

--- README.md
+++ README.md
@@ -1,11 +1,40 @@
-_**Typing Test**__
-A simple Python script to conduct a typing speed test and evaluate mistakes.
+_**Typing Speed Test Game**_
+This Python script allows users to play a typing speed test game. Users can choose to play the game, and the program will display a random sentence for them to type. After typing, it calculates the typing speed and the number of mistakes made.
 
-**Introduction**
-This Python script allows users to take a typing speed test. It randomly selects a sentence from a predefined set of sentences and measures the typing speed in words per second. Additionally, it calculates the number of mistakes made by the user.
+**Features**
+Displays a random sentence for the user to type.
+Calculates typing speed (words per second).
+Counts the number of mistakes made.
 
-**Usage**
--Make sure you have Python installed on your system.
--Have time module
--Have random module
 
+**How to Play**
+The program will prompt you with whether you want to play the game or not.
+
+If you choose to play (y), a random sentence will be displayed.
+
+Type the sentence exactly as shown.
+
+After typing, the program will display your typing speed and the number of mistakes.
+
+Repeat the process if you want to play again.
+
+Sample Gameplay
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~ Typing Test ~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Collect information about your geographical location which can be accurate to within several meters.
+
+Enter above sentence: Collect information about your geographical location which can be accurate to within several meters.
+
+Speed:  4.36 w/sec
+ALL Mistakes:  0
+Note
+If you want to exit the game, simply type n when prompted.
+Enjoy the typing speed test game! Happy typing!
+
+**Requirements**
+-Python 3.x
+-time module
+-random module
--- typingSpeedCalculater.py
+++ typingSpeedCalculater.py
@@ -1,52 +1,58 @@
-from time import *
+from time import time
 import random as r
 
-
-def mistake(para,userPara):
-    TotalmistakeWords=0
+def calculate_mistakes(para, userPara):
+    total_mistake_words = 0
     for i in range(len(para)):
         try:
-            if para[i]!=userPara[i]:
-                TotalmistakeWords+=1
+            if para[i] != userPara[i]:
+                total_mistake_words += 1
         except:
-            TotalmistakeWords+=1
-    return TotalmistakeWords
+            total_mistake_words += 1
+    return total_mistake_words
 
-
-def speed_time(time_s,time_e,userinput):
-    time_dalay = time_e - time_s
-    time_R = round(time_dalay,2)
-    speed=len(userinput)/time_R
-    return round(speed,2)
-    
+def calculate_speed(time_s, time_e, user_input):
+    time_delay = time_e - time_s
+    time_r = round(time_delay, 2)
+    speed = len(user_input) / time_r
+    return round(speed, 2)
 
 while True:
-    dis=input("Do you play game (y/n): ")
-    if dis=="y":
-        test=["The quick brown fox jumps over the lazy dog.",
-     "Collect information about your geographical location which can be accurate to within several meters.",
-      "Necessary cookies help make a website usable by enabling basic functions like page navigation and access to secure areas of the website. The website cannot function properly without these cookies.",
-        "Statistic cookies help website owners to understand how visitors interact with websites by collecting and reporting information anonymously.",
-       "Marketing cookies are used to track visitors across websites. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.",
-        "We and our advertising partners process your personal data using technology such as cookies in order to serve advertising, analyse our traffic and deliver customised experiences for you. You have a choice in who uses your data and for what purposes." ]
+    choice = input("Do you want to play a game (y/n): ")
+    
+    if choice.lower() == "y":
+        tests = [
+            "The quick brown fox jumps over the lazy dog.",
+            "Collect information about your geographical location which can be accurate to within several meters.",
+            "Necessary cookies help make a website usable by enabling basic functions like page navigation and access to secure areas of the website. The website cannot function properly without these cookies.",
+            "Statistic cookies help website owners to understand how visitors interact with websites by collecting and reporting information anonymously.",
+            "Marketing cookies are used to track visitors across websites. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.",
+            "We and our advertising partners process your personal data using technology such as cookies in order to serve advertising, analyse our traffic and deliver customised experiences for you. You have a choice in who uses your data and for what purposes."
+        ]
 
-        test1 =r.choice(test)
+        test1 = r.choice(tests)
         
-        print("~"*50)
-        print("~"*18+" Typing Test "+"~"*18)
-        print("~"*50)
+        print("~" * 50)
+        print("~" * 18 + " Typing Test " + "~" * 18)
+        print("~" * 50)
         print()
         print(test1)
         print()
         print()
-        time_s=time()
-        testinput=input("Enter above sentance : ")
-        time_e=time()
+        
+        time_start = time()
+        user_input = input("Enter the above sentence: ")
+        time_end = time()
 
-        print("Speed : ",speed_time(time_s,time_e,testinput)," w/sec")
-        print("ALL Mistaks : ",mistake(test1,testinput))
-    elif dis=="n":
-        print("~"*2+"Thank you for playing.You are play very well.~~")
+        speed = calculate_speed(time_start, time_end, user_input)
+        mistakes = calculate_mistakes(test1, user_input)
+
+        print("Speed:", speed, "w/sec")
+        print("All Mistakes:", mistakes)
+        
+    elif choice.lower() == "n":
+        print()
+        print("~" * 2 + "  Thank you for playing. You played very well.  ~~")
         break
     else:
-        print("you are enter wrong sign.Please enter only (y/n).")
+        print("You entered a wrong choice. Please enter only 'y' or 'n'.")
